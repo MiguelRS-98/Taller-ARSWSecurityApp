13c15cfdbac1a1dd2ca8affd7c7a6446fca8bae3
Miguel Rodríguez <miguel.rodriguez-s@mail.escuelaing.edu.co>
2021-07-08 11:25:33 -0500

    Security v1

---
+++ src/main/java/co/edu/escuelaing/interactivebalckboardlife/BBAppStarter.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.escuelaing.interactivebalckboardlife;
+
+
+import java.util.Collections;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author 
+ */
+@SpringBootApplication
+public class BBAppStarter {
+    /**
+     * 
+     * @param args 
+     */
+    public static void main(String[] args){
+        SpringApplication app = new SpringApplication(BBAppStarter.class);
+        
+        app.setDefaultProperties(Collections
+          .singletonMap("server.port", getPort()));
+        app.run(args);
+    }
+    /**
+     * 
+     * @return 
+     */
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 8080; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+}
